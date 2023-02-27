519f00750d0c040c33f84006d2f8a3cae3541d00
C P Singh <120619302+CPatgithub@users.noreply.github.com>
2023-02-27 13:08:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-var container=document.querySelector('.container'); // variable for container
-var value1=document.querySelector('.input'); // variable to store the 1st value of the input
+// Selecting the container which will show all the list of tasks
+var container=document.querySelector('.container');
+
+// Selecting the input field
+var value1=document.querySelector('.input'); 
+
+// Selecting the add button
 var add1=document.querySelector('.add');
+
+
 class item{
     constructor(name)
     {
@@ -10,16 +17,22 @@ class item{
     {
         var l1=document.createElement('div');
         l1.classList.add('item');
-        // changed input tag to span tag
+        
+        // inside the l1 div Creating div which contains task text and delete button also adding event on delete button called "remove"
         l1.innerHTML =`<div class="item"><span class="item_input">${name}</span><button onclick="removeEl(this)" class="remove"><i title="Delete task" class="fas fa-trash"></i></button></div>`
-        container.appendChild(l1); // So, User can add any no. of tasks
+        
+        // Showing l1 div inside the container div
+        container.appendChild(l1); 
     }
     remove(l1) 
     {
         container.removeChild(l1);
     }
 }
+
 container.innerHTML= localStorage.getItem("tasks");
+
+// Adding "onclick" & "Press enter" event on add button which call the function "check"
 add1.addEventListener('click', check);
 window.addEventListener('keydown',(e)=>{
     if(e.which==13) // 13 is the value for enter
@@ -27,6 +40,8 @@ window.addEventListener('keydown',(e)=>{
         check();
     }
 })
+
+// Writing Function which will be invoked during when add button is pressed
 function check()
 {
     if(value1.value!="") // Length of the input should be greater than 1
@@ -36,6 +51,8 @@ function check()
         value1.value="";
     }
 }
+
+// Writing Function which will be invoked when deleteAll button is pressed
 function deleteAll()
  {
     document.querySelector('.container').innerHTML="";
