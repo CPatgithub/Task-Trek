19c0611320005d51ea84ad8413cd81da37dd4b8f
C P Singh <120619302+CPatgithub@users.noreply.github.com>
2023-02-27 16:21:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,12 @@ var value1=document.querySelector('.input');
 // Selecting the add button
 var add1=document.querySelector('.add');
 
-
+// this "item" class is used to create new to-do list items by creating new HTML elements and adding them to the "container" div. 
+// It also provides a method to remove the HTML element from the "container" div.
 class item{
+    
+    // This is the constructor method of the "item" class, which is called when a new "item" object is created. 
+    // It takes a "name" parameter and calls the "create" method of the same class.
     constructor(name)
     {
         this.create(name);
@@ -18,18 +22,21 @@ class item{
         var l1=document.createElement('div');
         l1.classList.add('item');
         
-        // inside the l1 div Creating div which contains task text and delete button also adding event on delete button called "remove"
+        // inside the l1 div, Creating div which contains task text and delete button also adding event on delete button called "remove"
         l1.innerHTML =`<div class="item"><span class="item_input">${name}</span><button onclick="removeEl(this)" class="remove"><i title="Delete task" class="fas fa-trash"></i></button></div>`
         
         // Showing l1 div inside the container div
         container.appendChild(l1); 
     }
+    
+    // This is the "remove" method of the "item" class, which removes the HTML element passed as a parameter from the "container" div.
     remove(l1) 
     {
         container.removeChild(l1);
     }
 }
-
+// Retrives the value of the "tasks" key from the browser's local storage using the localStorage.getItem() method, and 
+// then sets the innerHTML property of the container element to that value.
 container.innerHTML= localStorage.getItem("tasks");
 
 // Adding "onclick" & "Press enter" event on add button which call the function "check"
@@ -41,7 +48,7 @@ window.addEventListener('keydown',(e)=>{
     }
 })
 
-// Writing Function which will be invoked during when add button is pressed
+// Writing Function which will be invoked when add button is pressed
 function check()
 {
     if(value1.value!="") // Length of the input should be greater than 1
@@ -60,10 +67,14 @@ function deleteAll()
 }
 
 // When Refreshing the page it should work accordingly
+// function setToLocalStore() is used to save the tasks present in the to-do list in the local storage of the browser. 
+// The localStorage property allows saving key/value pairs in a web browser and is used to store data without an expiration date.
 function setToLocalStore() {
     localStorage.setItem("tasks",document.querySelector('.container').innerHTML);
 }
+// When the delete button is clicked, the removeEl() function is called with "e" as the argument which is the button that was clicked.
 function removeEl(e){
     e.parentElement.remove();
     setToLocalStore();
+//     setToLocalStore() function is called to update the local storage with the new list of tasks after the deletion.
 }
